abc37ceed57fe61908e3169a971383bb6c0b2249
jocoly <jclyell@iu.edu>
2024-06-30 11:58:51 -0400

    adjusted routing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('output', express.static(path.join(__dirname, 'output')));
+app.use('/server/output', express.static(path.join(__dirname, 'output')));
 
 const PORT = 3000
 app.listen(PORT, () => {
