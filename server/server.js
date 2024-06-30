233aa8ffef33d7f001ce8dd02bd33c0d24f226d3
jocoly <jclyell@iu.edu>
2024-06-30 11:48:17 -0400

    express routing

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('output', express.static(path.join(__dirname, 'output')));
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
