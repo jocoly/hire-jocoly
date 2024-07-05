211b4c916c6cb6682463a249ff8c78b296181370
jocoly <jclyell@iu.edu>
2024-07-05 19:16:20 -0400

    added random prompts

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+// config goes here
+// will convert this to a module at some point and put it in a .config file
+
 const config = {
     BACKEND_ADDRESS:"https://major-vocal-vervet.ngrok-free.app",
     PORT:"5000",
@@ -9,45 +12,65 @@ const config = {
     SD_IMAGE_WIDTH:"512",
     SD_IMAGE_HEIGHT:"512",
 
-    NEGATIVE_PROMPT:"blurry, watermark, gross, disgusting, text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck"
+    NEGATIVE_PROMPT:"blurry, watermark, gross, disgusting, text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck",
+
+    RANDOM_PROMPT_ARRAY: [
+        "A baby elephant in a flower garden, wearing a crown of daisies",
+        "A whimsical owl perched on a crescent moon, stars twinkling around",
+        "A playful puppy splashing in a puddle, wearing a raincoat and boots",
+        "A chubby dragon blowing bubbles, surrounded by colorful flowers",
+        "A tiny mouse riding a leaf boat down a stream, holding a little sail",
+        "A curious fox peeking out from a magical forest, fireflies illuminating",
+        "A penguin sliding down a snowy hill, wearing a cozy scarf",
+        "A squirrel holding an umbrella, standing under a gentle rain shower",
+        "A rabbit painting colorful eggs, surrounded by spring blooms",
+        "A kitten napping in a hammock made of spider webs, in a fairy-tale garden",
+    ]
 }
 
-// add class navbarDark on navbar scroll
+// scroll to the top of the page on refresh
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+}
+
+// navbar is initially transparent on desktop displays
+// initially completely hidden on mobile displays
+// after scrolling, the nav bar turns opaque (for both mobile and desktop)
 const navBar = document.querySelector('.navbar');
 window.onscroll = async function () {
     const top = window.scrollY;
     if (top >= 100) {
         navBar.classList.add('navbarLight');
         navBar.classList.remove('navbarWhite');
-    } else if (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 1000) {
+    } else if (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 1000) { //checks for mobile viewport width (vw <1000)
         navBar.classList.add('navbarWhite');
     } else {
         navBar.classList.remove('navbarLight');
     }
 }
+
+// typewriter animation on the navbar brand name
 window.addEventListener('load', async function() {
     await writeLoop();
 })
 
+// check viewport width; for mobile or <1000 px width; don't load the header until the page is scrolled
 window.onload = function() {
     adjustNavBarVisibility()
-}
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const myName = "Joseph Colin Lyell"
 const typewriter = document.getElementById("typewriter");
 let sleepTime = 100;
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 async function writeLoop()  {
     for (let i=0; i< myName.length; i++) {
         typewriter.innerText = myName.substring(0, i+1);
         await sleep(sleepTime);
     }
 }
-
-
 function adjustNavBarVisibility() {
     let navBar = document.querySelector('.navbar');
     let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
@@ -60,10 +83,12 @@ function adjustNavBarVisibility() {
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
 
+// navbar collapse hamburger menu
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
 
+// gallery scroll buttons
 const buttonRight = document.getElementById('slideRight');
 const buttonLeft = document.getElementById('slideLeft');
 
@@ -75,6 +100,7 @@ buttonLeft.onclick = function () {
     document.getElementById('gallery').scrollLeft -= 532;
 }
 
+// image generation section (slides the rest of the page down when it loads)
 const promptSubmit = document.getElementById('promptSubmit');
 const randomSubmit = document.getElementById('randomSubmit');
 let loadingIcon = document.getElementById('loadingIcon');
@@ -85,13 +111,32 @@ promptSubmit.onclick = async function () {
     await generateImage(prompt);
 }
 
+randomSubmit.onclick = async function () {
+    let prompt = config.RANDOM_PROMPT_ARRAY[Math.floor(Math.random()*config.RANDOM_PROMPT_ARRAY.length)];
+    await generateImage(prompt);
+}
+function adjustBannerHeight() {
+    let banner = document.getElementById("home");
+    banner.style.minHeight = "1742px";
+}
 
+const gallery = document.getElementById("gallery");
+const slideLeft = document.getElementById("slideLeft");
+const slideRight = document.getElementById("slideRight");
+function repositionGallery() {
+    gallery.style.top = "1012px";
+    slideLeft.style.top = "1262px";
+    slideRight.style.top = "1262px";
+}
+
+
+// calling the 'callBackendPipeline' function to send the prompt to the server and return an image url
+const requestTimeoutSeconds = 600000
 async function generateImage(prompt) {
     try {
         let pipeline = "StableDiffusion"
         let numImages = 1;
         let imgUrl = "";
-        let loadingIcon = document.getElementById('loadingIcon');
         let generatedImage = document.getElementById('generatedImage');
         let generatedImageUrl;
         try {
@@ -117,23 +162,6 @@ async function generateImage(prompt) {
         console.error("Error generating image:", error);
     }
 }
-
-function adjustBannerHeight() {
-    let banner = document.getElementById("home");
-    banner.style.minHeight = "1742px";
-}
-
-function repositionGallery() {
-    let gallery = document.getElementById("gallery");
-    let slideLeft = document.getElementById("slideLeft");
-    let slideRight = document.getElementById("slideRight");
-    gallery.style.top = "1012px";
-    slideLeft.style.top = "1262px";
-    slideRight.style.top = "1262px";
-}
-
-const requestTimeoutSeconds = 600000
-
 async function callBackendPipeline(prompt, pipeline, numImages, imgUrl) {
     const start_time = new Date();
     const backendUrl = config.BACKEND_ADDRESS;
